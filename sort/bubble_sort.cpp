c6c018a044cc1959457ebc82d7d36e2a0fc64512
Flyoung <1011475248@qq.com>
2021-12-03 21:41:14 +0800

    first commit

---
+++ sort/bubble_sort.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+void bubble_sort(int a[],int n)
+{
+    int tmp;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){     //j<n-i是为了减少重复遍历
+            if(a[j]>a[j+1])
+                swap(a[j],a[j+1]);
+        }
+    }
+}
